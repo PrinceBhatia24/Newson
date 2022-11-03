ae095d1d070cbb2e52dda4cc71695132cb41f5e5
Prince Bhatia <Princebhatia10021@gmail.com>
2022-11-03 10:18:07 +0530

    Added News Using Api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import Navbar from './Components/Navbar'
 import News from './Components/News';
 
+
 export default class App extends Component {
   render() {
     return (
