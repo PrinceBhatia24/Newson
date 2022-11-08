5c2e8a2d41a4add064d649a7e2dbf5addc1c4969
Prince Bhatia <Princebhatia10021@gmail.com>
2022-11-08 09:39:19 +0530

    ccd

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import News from "./Components/News";
 import Navbar from "./Components/Navbar";
+import { Link } from "react-router-dom";
 
 const App = (props) => {
-  const apiKey = "e61d86b6a1174866b569d2dd57e73549";
+  const apiKey = "01cb608311c147f7a72d2f1cd1148ae0";
   const pageSize = "16"
   const country = "in"
+  
 
   return (
     <div>
       <Router>
         <Navbar/>
+
         <Switch>
           <Route exact path="/Newson">
             {" "}
@@ -100,3 +103,269 @@ const App = (props) => {
   );
 };
 export default App;
+
+// import "./App.css";
+// import React ,{useState,useEffect} from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import News from "./Components/News";
+// import { Link } from "react-router-dom";
+// import LoadingSpinner from "./Components/LoadingSpinner";
+// import axios from "axios";
+// import NewsItem from "./Components/NewsItem";
+
+// const App = (props) => {
+//   const [coun , setCoun] =useState("in")
+//   const [cat , setCat] =useState("general")
+//   const [loading, setLoading] = useState(false) 
+//   const [page, setPage] = useState(1)
+//   const [totalResults, setTotalResults] = useState(0)
+//   const [articles, setArticles] = useState([])
+//   const apiKey = "e61d86b6a1174866b569d2dd57e73549";
+//   const pageSize = "16"
+//   console.log(coun)
+
+// const getData= async()=>{
+  
+//   setLoading(true)
+//   const res = await axios.get(
+//     `https://newsapi.org/v2/top-headlines?country=${coun}&category=${cat}&apiKey=${apiKey}&page=1&pageSize=${pageSize}`
+//     );
+//     setLoading(false)
+//     setTotalResults(res.data.totalResults)
+//     setArticles(res.data.articles)
+// }
+
+// const handleChangeNext = async () => {
+//   if (!(page + 1 > Math.ceil({ totalResults } / pageSize))) {
+//     setLoading(true)
+//     const res = await axios.get(
+//       `https://newsapi.org/v2/top-headlines?country=${coun}&category=${cat}&apiKey=${apiKey}&page=${page + 1}&pageSize=${pageSize}`
+//     );
+//     setArticles(res.data.articles)
+//     setPage(page + 1)
+//     setLoading(false)
+//   }
+// }
+
+// const handleChangePrevious = async () => {
+//   setLoading(true)
+//   const res = await axios.get(
+//     `https://newsapi.org/v2/top-headlines?country=${coun}&category=${cat}&apiKey=${apiKey}&page=${page - 1}&pageSize=${pageSize}`
+//   );
+//   setArticles(res.data.articles)
+//   setPage(page - 1)
+//   setLoading(false)
+// }
+
+
+// useEffect(() => {
+//   getData()
+// }, [])
+
+
+//   return (
+//     <div>
+//       <Router>
+//         {/* <Navbar/> */}
+//         <div>
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+         
+//           <div className="container-fluid">
+//             <Link className="navbar-brand"  to="#">
+//             <img src={process.env.PUBLIC_URL + '/images/p.jpg'} alt="Newson"   width="100" height="20" style={{marginLeft:20}}></img>
+//             </Link>
+//             <button
+//               className="navbar-toggler"
+//               type="button"
+//               data-bs-toggle="collapse"
+//               data-bs-target="#navbarSupportedContent"
+//               aria-controls="navbarSupportedContent"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation"
+//             >
+//               <span className="navbar-toggler-icon" />
+//             </button>
+//             <div
+//               className="collapse navbar-collapse"
+//               id="navbarSupportedContent"
+//             >
+//               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                 <li className="nav-item">
+//                   <Link className="nav-link active" aria-current="page" to="/">
+//                     Home
+//                   </Link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <link
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     to="/sports"
+//                   >
+//                     Sports
+//                   </link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <Link
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     to="/entertainment"
+//                   >
+//                     Entertainment
+//                   </Link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <Link
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     to="/health"
+//                   >
+//                     Health
+//                   </Link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <Link
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     to="/science"
+//                   >
+//                     Science
+//                   </Link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <Link
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     to="/business"
+//                   >
+//                     Business
+//                   </Link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <Link
+//                     className="nav-link active"
+//                     aria-current="page"
+//                     to="/technology"
+//                   >
+//                     Technology
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <select className="form-select c1" aria-label="Default select example" >
+                    
+//                     <option value="in">India</option> 
+//                     <option value="us">United State</option>
+//                     <option value="ae">United arab emirates</option>
+//                     <option value="nl">Netherland</option>
+//                     <option value="br">Brazil</option>
+//                   </select>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </nav>
+//       </div>
+//         <Switch>
+//           <Route exact path="/Newson">
+//             {" "}
+//             <News
+//               key="general"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"General"}
+//             />
+//           </Route>
+//           <Route exact path="/">
+//             {" "}
+//             <News
+//               key="general"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"General"}
+//             />
+//           </Route>
+//           <Route exact path="/sports">
+//             {" "}
+//             <News
+//               key="sports"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"Sports"}
+//             />
+//           </Route>
+//           <Route exact path="/business">
+//             {" "}
+//             <News
+//               key="business"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"Business"}
+//             />
+//           </Route>
+//           <Route exact path="/health">
+//             {" "}
+//             <News
+//               key="health"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"Health"}
+//             />
+//           </Route>
+//           <Route exact path="/science">
+//             {" "}
+//             <News
+//               key="science"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"Science"}
+//             />
+//           </Route>
+//           <Route exact path="/technology">
+//             {" "}
+//             <News
+//               key="technology"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"Technology"}
+//             />
+//           </Route>
+//           <Route exact path="/entertainment">
+//             {" "}
+//             <News
+//               key="entertainment"
+//               pageSize={pageSize}
+//               apiKey={apiKey}
+//               country={coun}
+//               category={"Entertainment"}
+//             />
+//           </Route>
+//         </Switch>
+//       </Router>
+//       <h3 style={{ marginTop: 89, marginLeft: 20 }}>Top Headlines  - {cat} {loading && <LoadingSpinner />} </h3>
+//       <div className="container-fluid my-5 d-flex" id="sidebar">
+//         {/* {this.state.loading && <LoadingSpinner/>} */}
+//         {/* <SideBar/> */}
+//         <div className="row" style={{ marginLeft: 24 }}>
+//           {!loading && articles.map((element) => {
+//             return <div className="col-md-3" key={element.url}>
+//               <NewsItem author={element.author} date={element.publishedAt} title={element.title ? element.title.slice(0, 60) : ""} description={element.description ? element.description.slice(0, 100) : ""} url={element.urlToImage} newsurl={element.url} />
+//             </div>
+//           })}
+//         </div>
+//       </div>
+//       <div className="container d-flex justify-content-between">
+//         <button onClick={handleChangePrevious} disabled={page <= 1} className="btn btn-dark">Previous</button>
+//         <button disabled={page + 1 > Math.ceil(totalResults / pageSize)} onClick={handleChangeNext} className="btn btn-dark">Next</button>
+//       </div>
+//     </div>
+    
+    
+//   );
+// };
+// export default App;
--- src/Components/News.js
+++ src/Components/News.js
@@ -58,7 +58,19 @@ const News = (props) => {
 
   return (
     <>
-      <h3 style={{ marginTop: 89, marginLeft: 20 }}>Top Headlines - {props.category} {loading && <LoadingSpinner />} </h3>
+      <a
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvas"
+        style={{ position: "absolute", left: 53, top: 93, listStyleType: "none" }}
+        
+      >
+        <li>
+          <div className="div"></div>
+          <div className="div"></div>
+          <div className="div "></div>
+        </li>
+      </a>
+      <h3 style={{ marginTop: 89, marginLeft: 65 }}>Top Headlines - {props.category} {loading && <LoadingSpinner />} </h3>
       <div className="container-fluid my-5 d-flex" id="sidebar">
         {/* {this.state.loading && <LoadingSpinner/>} */}
         {/* <SideBar/> */}
@@ -74,7 +86,125 @@ const News = (props) => {
         <button onClick={handleChangePrevious} disabled={page <= 1} className="btn btn-dark">Previous</button>
         <button disabled={page + 1 > Math.ceil(totalResults / props.pageSize)} onClick={handleChangeNext} className="btn btn-dark">Next</button>
       </div>
+      <div className="offcanvas offcanvas-start bg-light text-dark" id="offcanvas" style={{ width: 300 }}>
+        <div className="offcanvas-body" >
+          <div
+            style={{ backgroundColor: "rgb(33 37 41)", position: "absolute", left: 0, top: 0, height: 56 }}
+            className="container-fluid"
+          >
+            <ul className="flex3 mt-2 mb-2">
+              <li>
+                <img
+                  src={process.env.PUBLIC_URL + '/images/p.jpg'} alt="Newson" height="20"
+                  style={{ marginLeft: 26 }}
+                  width="140px"
 
+                />
+              </li>
+              {/* <li><button class="btn" style="margin-left: 15px; background-color: #2874f0;"
+                          data-bs-dismiss="offcanvas"><img
+                              src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTUiIGhlaWdodD0iMTUiIHZpZXdCb3g9IjAgMCAxNSAxNSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMS4wNjMgMWwxMi4zIDEyLjQyNW0uMDYzLTEyLjM2MkwxIDEzLjM2MyIgc3Ryb2tlPSIjZmZmIiBzdHJva2Utd2lkdGg9IjEuNSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBmaWxsPSJub25lIi8+PC9zdmc+"></button>
+                  </li> */}
+            </ul>
+          </div>
+          <form action="">
+            <div>
+              <ul
+                style={{ marginTop: 80, listStyle: "none", marginLeft: "-35px" }}
+              >
+                <li>
+                  <h4>Login to check your account</h4>
+                </li>
+                <li>
+                  <div className="mb-2">
+                    <label htmlFor="email" className="form-label">
+                      Enter Email:
+                    </label>
+                    <input
+                      type="email"
+                      placeholder=" Enter Email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      required=""
+                    />
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-1">
+                    <label htmlFor="password" className="form-label">
+                      Enter Password:
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="Enter Password"
+                      className="form-control mb-5"
+                      id="password"
+                      name="password"
+                      required=""
+                    />
+                  </div>
+                </li>
+                <li>
+                  <p
+                    style={{
+                      fontSize: 13,
+                      cursor: "default",
+                      textAlign: "center"
+                    }}
+                  >
+                    By Continuing, you agree to flipkart's
+                    <span className="text-primary">Term of Use</span> and{" "}
+                    <span className="text-primary">Privacy</span>
+                  </p>
+                </li>
+                <li>
+                  <div className="container-fluid text-center">
+                    <button
+                      className="btn btn-primary px-5"
+                      type="submit"
+                      id="loginbtn2"
+                      style={{ backgroundColor: "#fb641b" }}
+                    >
+                      Login
+                    </button>
+                  </div>
+                </li>
+                <li>
+                  <p style={{ textAlign: "center", marginTop: 20, fontSize: 20 }}>
+                    Or
+                  </p>
+                  <div className="container-fluid text-center bg-light">
+                    <button
+                      className="btn btn-light text-primary px-4 pt-2"
+                      type="button"
+                    >
+                      Request OTP
+                    </button>
+                    <hr />
+                  </div>
+                </li>
+                <li>
+                  <center>
+                    {" "}
+                    <a
+                      className="m-3"
+                      // onClick={DisplayChange2}
+                      style={{
+                        textDecoration: "none",
+                        cursor: "pointer",
+                        fontSize: 13
+                      }}
+                    >
+                      New to Flipkart ? Create an account
+                    </a>
+                  </center>
+                </li>
+              </ul>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
